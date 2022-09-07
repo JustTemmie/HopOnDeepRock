9786ef8093f717b926ef69c7e595e6c357acaeb9
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-09-07 23:48:56 +0200

    more games

--- HopOnDeepRockWindows.plugin.js
+++ HopOnDeepRockWindows.plugin.js
@@ -2,7 +2,7 @@
  * @name HopOnDeepRock
  * @author Temmie
  * @description Forces you into hopping on deep rock when your friends demand it <3
- * @version 0.0.1
+ * @version 0.0.2
  * @authorId 725539745572323409
  * @donate https://paypal.me/TemmieTem
  * @source https://github.com/JustTemmie/HopOnDeepRock/
@@ -10,15 +10,38 @@
 
 const execSync = require('child_process').execSync;
 
+// var config = {
+//   "ignoredUsers": [],
+//   "games": {
+//       "gameID": "538430",
+//       "gameQuote": "hop on deep rock",
+//   }
+// };
+
+const config = {
+  info: {
+      name: "HopOnDeepRock",
+      authors: [{
+          name: "Temmie"
+      }],
+      version: "0.0.2",
+      description: "Forces you into hopping on deep rock when your friends demand it <3",
+      github_raw: "https://github.com/JustTemmie/HopOnDeepRock/blob/main/HopOnDeepRock.plugin.js",
+  },
+  changelog: [
+      {
+          title : "v0.0.2",
+          items: ["Added settings.",
+                  "Fixed random crashing."],
+      }
+  ]
+};
+
 module.exports = (() => {
-  const config = {"info":{"name":"Hop On Deep Rock","authors":[{"name":"Temmie","discord_id":"725539745572323409","github_username":"JustTemmie",}],"version":"0.0.1","description":"Deep Rock?!?!", "main":"index.js"}};
+  //const config = {"info":{"name":"Hop On Deep Rock","authors":[{"name":"Temmie","discord_id":"725539745572323409","github_username":"JustTemmie",}],"version":"0.0.1","description":"Forces you into hopping on deep rock when your friends demand it <3", "main":"index.js"}};
 
   return !global.ZeresPluginLibrary ? class {
       constructor() {this._config = config;}
-      getName() {return config.info.name;}
-      getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
-      getDescription() {return config.info.description;}
-      getVersion() {return config.info.version;}
       load() {
           BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
               confirmText: "Download Now",
@@ -59,7 +82,10 @@ module.exports = (() => {
       let { message } = event;
       if (message.author.bot) return;
       if (message.author.id === this.userId) return;
-      if (message.content.includes("hop on deep rock")) execSync('start steam://rungameid/548430', { encoding: 'utf-8' });
+      if (message.content.includes("hop on deep rock")) execSync('start steam://rungameid/548430');
+      if (message.content.includes("hop on bloons")) execSync('start steam://rungameid/960090');
+      if (message.content.includes("hop on risk of rain")) execSync('start steam://rungameid/632360');
+      if (message.content.includes("hop on among us")) execSync('start steam://rungameid/945360');
     }
 
     onStop() {
@@ -72,7 +98,4 @@ module.exports = (() => {
       return plugin(Plugin, Api);
   })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
-  // Dispatcher.subscribe("MESSAGE_CREATE", this.onMessage);
-  // onMessage({ message }) {
-  //   BdApi.alert("HEY!", "Ready to play some deeprock?!!?!?!!?");
-  // }
+  // Dispa=
